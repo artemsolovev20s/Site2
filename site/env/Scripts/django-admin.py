12612b74414991151e187f04839428471bd85399
the_7th_cavalry <Max99pain@rambler.ru>
2020-06-06 20:37:55 +0300

    first commit

---
+++ site/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\datim\OneDrive\Рабочий стол\firma\firma\firma\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
